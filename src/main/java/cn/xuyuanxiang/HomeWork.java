5f10b57bc8f6c05a4065bf6b6cacaaa981fcc130
baba_xu <1459160492@qq.com>
2020-07-26 13:06:58 +0800

    first

---
+++ src/main/java/cn/xuyuanxiang/HomeWork.java
@@ -0,0 +1,11 @@
+package cn.xuyuanxiang;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+}
+
---
+++ src/main/java/cn/xuyuanxiang/HomeWork2.java
@@ -0,0 +1,7 @@
+package cn.xuyuanxiang;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+    }
+}
