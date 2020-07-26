66242b541036c3d7171751d18bf92167ea3b7590
baba_xu <1459160492@qq.com>
2020-07-26 13:09:48 +0800

    second

--- src/main/java/cn/xuyuanxiang/HomeWork2.java
+++ src/main/java/cn/xuyuanxiang/HomeWork2.java
@@ -2,6 +2,12 @@ package cn.xuyuanxiang;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-
+        System.out.println("hahahahahahah");
+        System.out.println("hahahahahahah");
+        System.out.println("hahahahahahah");
+        System.out.println("hahahahahahah");
+        System.out.println("hahahahahahah");
+        System.out.println("hahahahahahah");
+        System.out.println("hahahahahahah");
     }
 }
